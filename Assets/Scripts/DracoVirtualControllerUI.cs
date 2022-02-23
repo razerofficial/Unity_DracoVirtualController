34fb66fdd635331e759bff9298892112cc98653c
Timothy Graupmann <tgraupmann@gmail.com>
2022-02-23 09:57:19 -0800

    Add meta references

--- Assets/Scripts/DracoVirtualControllerUI.cs
+++ Assets/Scripts/DracoVirtualControllerUI.cs
@@ -1,9 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DracoVirtualControllerUI : MonoBehaviour
 {
+    public Image _mImageA = null;
+    public Image _mImageB = null;
+    public Image _mImageX = null;
+    public Image _mImageY = null;
+    public Image _mImageDpadDown = null;
+    public Image _mImageDpadLeft = null;
+    public Image _mImageDpadRight = null;
+    public Image _mImageDpadUp = null;
+    public Image _mImageLeftBumper = null;
+    public Image _mImageLeftStick = null;
+    public Image _mImageLeftThumb = null;
+    public Image _mImageLeftTrigger = null;
+    public Image _mImageRightBumper = null;
+    public Image _mImageRightStick = null;
+    public Image _mImageRightThumb = null;
+    public Image _mImageRightTrigger = null;
+    public Image _mImageTap = null;
+    public Image _mImageSelect = null;
+    public Image _mImageStart = null;
+
     // Start is called before the first frame update
     void Start()
     {
